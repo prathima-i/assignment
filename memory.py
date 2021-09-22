485b480d77d6a011c1be57dde9104d5fb12c7db3
prathima-i <91138467+prathima-i@users.noreply.github.com>
2021-09-22 16:22:33 +0530

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,1 +1,6 @@
-
+"""1.	Take the input from the user for(Total number of people,Number of seats for bus. Based on two inputs
+Decide how many number of buses required"""
+no_of_people=int(input("total no.of people:"))
+no_of_seats=int(input("no.of seats:"))
+no_of_buses=no_of_people//no_of_seats
+print(no_of_buses)
