a3145be7963afae1d7f6c5e8fb7183f0f9dda324
prathima-i <91138467+prathima-i@users.noreply.github.com>
2021-09-22 16:55:05 +0530

    Update memory.py

--- memory.py
+++ memory.py
@@ -4,3 +4,9 @@ no_of_people=int(input("total no.of people:"))
 no_of_seats=int(input("no.of seats:"))
 no_of_buses=no_of_people//no_of_seats
 print(no_of_buses)
+"""
+2.take temperature from the user and convert fahrenheit -> Celsius.
+"""
+f=14
+c=(f-32)/1.8
+print(c)
